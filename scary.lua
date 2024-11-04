cf538d235667f2adb92ecd115e79ab4391f9afc5
buhh <108026120+buhhhhg@users.noreply.github.com>
2024-11-04 20:56:44 +0500

    Update scary.lua

--- scary.lua
+++ scary.lua
@@ -3,12 +3,16 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TextChatService = game:GetService("TextChatService")
 local Players = game:GetService("Players")
 local StarterGui = game:GetService("StarterGui")
+local GuiService = game:GetService("GuiService")
+local TeleportService = game:GetService("TeleportService")
 
 local LocalPlayer = Players.LocalPlayer
 
 local isLegacyChat = TextChatService.ChatVersion == Enum.ChatVersion.LegacyChatService
 
 local Red = Instance.new("ColorCorrectionEffect", Lighting)
+
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 
 local coreGuiTypeNames = {
     Enum.CoreGuiType.Backpack,
@@ -29,6 +33,22 @@ function chatMessage(str)
     end
 end
 
+Players.LocalPlayer.OnTeleport:Connect(function(State)
+    if queueteleport then
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/buhhhhg/RobloxScriptz/refs/heads/main/scary.lua'))()")
+        queueteleport("while task.wait() do warn('Thought you could escape?') end")
+    end
+
+    pcall(function() GuiService:ClearError() end)
+    local s, r = pcall(function() TeleportService:TeleportCancel() end)
+
+    if s then
+        for i=1,3 do
+            chatMessage("Nice try. You can't escape.")
+        end
+    end
+end)
+
 function randstr(length)
     local a = ''
     for i = 1, length do
@@ -42,11 +62,12 @@ Red.Enabled = true
 Red.Saturation = -1
 Red.Contrast = -2558222
 Red.Brightness = -1555
+Red.TintColor = Color3.fromRGB(255, 0, 0)
 
 task.spawn(function()
     while task.wait(0.01) do
         for _, guiType in pairs(coreGuiTypeNames) do
-            pcall(function() StarterGui:SetCore(guiType, false) end)
+            pcall(function() StarterGui:SetCoreGuiEnabled(guiType, false) end)
         end
     end
 end)
@@ -63,12 +84,18 @@ task.spawn(function()
     end
 end)
 
+task.spawn(function()
+    while task.wait() do
+        Red.Saturation = math.random(-1, 0)
+    end
+end)
+
 for i=1,3 do
     chatMessage('You cheated.')
 end
 
 task.spawn(function()
-    while task.wait(1) do
+    while task.wait(12) do
         for i=1,3 do
             chatMessage('You cheated.')
         end
@@ -95,12 +122,5 @@ task.spawn(function()
                 Humanoid.JumpHeight = 0
             end
         end
-    end
-end)
-
-LocalPlayer.OnTeleport:Connect(function(State)
-    qtp = queueteleport or queue_teleport or function(...) end
-    if qtp then
-        qtp("loadstring(game:HttpGet('https://raw.githubusercontent.com/buhhhhg/RobloxScriptz/refs/heads/main/scary.lua'))()")
     end
 end)
